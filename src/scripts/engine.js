524c4f6814299812109a6dc16db55d92f8ca7ad3
João Pedro E. Silveira <108689656+jpsilveira11@users.noreply.github.com>
2023-11-28 20:37:35 -0300

    adding tunes via keyboard

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -5,8 +5,15 @@ let audio = new Audio("./src/tunes");
 const playTune=(key)=>{
     audio.src=`./src/tunes/${key}.wav`;
     audio.play();
+    const clickedKey=document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(()=>{clickedKey.classList.remove("active");},150);
 };
 
 keys.forEach((key)=>{
     key.addEventListener("click",()=>playTune(key.dataset.key));
+});
+
+document.addEventListener("keydown",(e)=>{
+    playTune(e.key);
 });
