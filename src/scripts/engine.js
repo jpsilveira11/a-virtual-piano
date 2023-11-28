67c618205459a3e9edbe1f8223ab1469a77cfb21
João Pedro E. Silveira <108689656+jpsilveira11@users.noreply.github.com>
2023-11-28 20:27:11 -0300

    key tunes

---
+++ src/scripts/engine.js
@@ -0,0 +1,12 @@
+const keys=document.querySelectorAll(".piano-keys .key");
+
+let audio = new Audio("./src/tunes");
+
+const playTune=(key)=>{
+    audio.src=`./src/tunes/${key}.wav`;
+    audio.play();
+};
+
+keys.forEach((key)=>{
+    key.addEventListener("click",()=>playTune(key.dataset.key));
+});
